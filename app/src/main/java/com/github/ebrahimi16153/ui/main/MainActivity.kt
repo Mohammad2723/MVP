bc33cbb4a81d47afb412a93c0a9d614a8fe52c68
mohammad2723 <kotlin2723@gmail.com>
2024-05-15 16:44:54 +0330

    1->move main activity to ui package

--- app/src/main/java/com/github/ebrahimi16153/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/ebrahimi16153/ui/main/MainActivity.kt
@@ -1,15 +1,12 @@
-package com.github.ebrahimi16153
+package com.github.ebrahimi16153.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.github.ebrahimi16153.databinding.ActivityMainBinding
 
-private lateinit var  binding: ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
